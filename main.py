744c1d6cdcdc51e6ca86b51d3d2e1dfa3ddcc608
tsion mulugeta <tsionmulugeta1111@gmail.com>
2026-05-22 11:32:44 +0300

    randomize

--- main.py
+++ main.py
@@ -33,6 +33,19 @@ maze_complete = False
 
 entrance_created = False
 
+# solver variables
+# solver_row = 0
+# solver_col = 0
+
+# solver_stack = []
+
+# solver_visited = set()
+
+# dead_ends = []
+
+# solver_started = False
+# solver_finished = False
+
 
 #visited set to keep track of which cells have been visited during maze generation
 visited = set()
@@ -65,6 +78,19 @@ def remove_wall(current_row, current_col, next_row, next_col):
     # move RIGHT
     elif next_col == current_col + 1:
         east_wall[current_row][current_col + 1] = 0
+
+#for the solver wall check functions
+# def can_move_up(row, col):
+#     return north_wall[row][col] == 0
+
+# def can_move_down(row, col):
+#     return north_wall[row + 1][col] == 0
+
+# def can_move_left(row, col):
+#     return east_wall[row][col] == 0
+
+# def can_move_right(row, col):
+#     return east_wall[row][col + 1] == 0
 
 # visualize the visited cells during maze generation        
 def draw_visited():
@@ -146,6 +172,31 @@ def get_unvisited_neighbors(row, col):
 
     return neighbors
 
+# def get_solver_neighbors(row, col):
+
+#     neighbors = []
+
+#     # UP
+#     if in_bounds(row - 1, col):
+#         if can_move_up(row, col) and (row - 1, col) not in solver_visited:
+#             neighbors.append((row - 1, col))
+
+#     # DOWN
+#     if in_bounds(row + 1, col):
+#         if can_move_down(row, col) and (row + 1, col) not in solver_visited:
+#             neighbors.append((row + 1, col))
+
+#     # LEFT
+#     if in_bounds(row, col - 1):
+#         if can_move_left(row, col) and (row, col - 1) not in solver_visited:
+#             neighbors.append((row, col - 1))
+
+#     # RIGHT
+#     if in_bounds(row, col + 1):
+#         if can_move_right(row, col) and (row, col + 1) not in solver_visited:
+#             neighbors.append((row, col + 1))
+
+#     return neighbors
 
 # move to the next cell in the depth-first search algorithm, removing the wall between the current cell and the next cell. 
 def generate_maze_step():
@@ -171,6 +222,42 @@ def generate_maze_step():
     if not stack:
         maze_complete = True
         
+# def move_solver():
+
+#     global solver_row, solver_col, solver_finished
+
+#     if solver_finished:
+#         return
+
+#     neighbors = get_solver_neighbors(solver_row, solver_col)
+
+#     # MOVE FORWARD
+#     if neighbors:
+
+#         next_row, next_col = neighbors[0]
+
+#         solver_stack.append((next_row, next_col))
+
+#         solver_row = next_row
+#         solver_col = next_col
+
+#         solver_visited.add((solver_row, solver_col))
+
+#     # DEAD END → BACKTRACK
+#     else:
+
+#         dead_ends.append((solver_row, solver_col))
+
+#         solver_stack.pop()
+
+#         if solver_stack:
+#             solver_row, solver_col = solver_stack[-1]
+
+#     # CHECK EXIT
+#     if solver_col == C - 1 and east_wall[solver_row][C] == 0:
+#         solver_finished = True
+        
+
 
 def draw_maze():
     # draw internal walls
@@ -227,6 +314,60 @@ def draw_maze():
                 2
             ) 
 
+# def draw_entrance():
+#     text = font.render("START", True, (0, 150, 0))
+
+#     x = START_X + 5
+#     y = START_Y + entrance_row * CELL_SIZE + CELL_SIZE // 2 - 10
+
+#     screen.blit(text, (x, y))
+
+
+# def draw_exit():
+#     text = font.render("END", True, (200, 0, 0))
+
+#     x = START_X + (C - 1) * CELL_SIZE + 5
+#     y = START_Y + exit_row * CELL_SIZE + CELL_SIZE // 2 - 10
+
+#     screen.blit(text, (x, y))
+
+# def draw_solver():
+
+#     x = START_X + solver_col * CELL_SIZE
+#     y = START_Y + solver_row * CELL_SIZE
+
+#     pygame.draw.rect(
+#         screen,
+#         (0, 255, 0),
+#         (x, y, CELL_SIZE, CELL_SIZE)
+#     )
+    
+# def draw_dead_ends():
+
+#     for row, col in dead_ends:
+
+#         x = START_X + col * CELL_SIZE
+#         y = START_Y + row * CELL_SIZE
+
+#         pygame.draw.rect(
+#             screen,
+#             (100, 100, 255),
+#             (x, y, CELL_SIZE, CELL_SIZE)
+#         )
+        
+# def draw_solution_path():
+
+#     for row, col in solver_stack:
+
+#         x = START_X + col * CELL_SIZE
+#         y = START_Y + row * CELL_SIZE
+
+#         pygame.draw.rect(
+#             screen,
+#             (255, 255, 0),  # yellow
+#             (x, y, CELL_SIZE, CELL_SIZE)
+#         )
+
 # set the frames per second for the game loop 
 FPS = 2
 
@@ -247,14 +388,41 @@ while running:
         east_wall[exit_row][C] = 0
 
         entrance_created = True
+    
+    # if maze_complete and entrance_created and not solver_started:
+
+    #     solver_row = entrance_row
+    #     solver_col = 0
+
+    #     solver_stack.append((solver_row, solver_col))
+    #     solver_visited.add((solver_row, solver_col))
+
+    #     solver_started = True
+    
+    # if maze_complete and solver_started:
+    #     move_solver()
+
 
     draw_visited()
+    
+    # draw_dead_ends()
+    
+    # if solver_finished:
+    #     draw_solution_path()
+    
     draw_current_cell()
-    draw_coordinates()
-    draw_maze()
+    
+    # if solver_started:
+    #     draw_solver()
+    
+    # draw_coordinates()
+    
+    # if entrance_created:
+    #     draw_entrance()
+    #     draw_exit()
+    
+    # draw_maze()
 
-    
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running=False
