e11464ed08734e2dc841558cb58cdc49c7458518
tsion mulugeta <tsionmulugeta1111@gmail.com>
2026-05-22 13:05:47 +0300

    fix indentation error

--- main.py
+++ main.py
@@ -334,26 +334,26 @@ def draw_exit():
 def draw_solver():
 
     x = START_X + solver_col * CELL_SIZE
-     y = START_Y + solver_row * CELL_SIZE
+    y = START_Y + solver_row * CELL_SIZE
 
-     pygame.draw.rect(
-         screen,
-         (0, 255, 0),
-         (x, y, CELL_SIZE, CELL_SIZE)
-     )
-    
- def draw_dead_ends():
+    pygame.draw.rect(
+        screen,
+        (0, 255, 0),
+        (x, y, CELL_SIZE, CELL_SIZE)
+    )
 
-     for row, col in dead_ends:
+def draw_dead_ends():
 
-         x = START_X + col * CELL_SIZE
-         y = START_Y + row * CELL_SIZE
+    for row, col in dead_ends:
 
-         pygame.draw.rect(
-             screen,
-             (100, 100, 255),
-             (x, y, CELL_SIZE, CELL_SIZE)
-         )
+        x = START_X + col * CELL_SIZE
+        y = START_Y + row * CELL_SIZE
+
+        pygame.draw.rect(
+            screen,
+            (100, 100, 255),
+            (x, y, CELL_SIZE, CELL_SIZE)
+        )
         
 def draw_solution_path():
 
